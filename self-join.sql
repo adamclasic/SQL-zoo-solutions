1ba17c4545dc43a0529709e9883cfb3afb72fbe9
adam <aabou9@gmail.com>
2020-05-12 18:49:43 +0100

    finish self join

--- self-join.sql
+++ self-join.sql
@@ -52,3 +52,54 @@ FROM route a JOIN route b ON
   JOIN stops stopa ON (a.stop=stopa.id)
   JOIN stops stopb ON (b.stop=stopb.id)
 WHERE stopa.name='Craiglockhart'AND stopb.name = 'London Road'
+
+--#7
+/*
+Give a list of all the services which connect stops 115 and 137 ('Haymarket' and 'Leith')
+*/
+SELECT DISTINCT a.company, a.num
+FROM route a JOIN route b ON
+  (a.company =b.company AND a.num=b.num)
+  JOIN stops stopa ON (a.stop=stopa.id)
+  JOIN stops stopb ON (b.stop=stopb.id)
+WHERE stopa.name='Haymarket' AND stopb.name='Leith'
+
+--#8
+/*
+Give a list of the services which connect the stops 'Craiglockhart' and 'Tollcross'
+*/
+SELECT DISTINCT a.company, a.num
+FROM route a
+  JOIN route b ON (a.num=b.num AND a.company=b.company)
+  JOIN stops stopa ON (a.stop=stopa.id)
+  JOIN stops stopb ON (b.stop=stopb.id)
+WHERE stopa.name = 'Craiglockhart' AND stopb.name = 'Tollcross'
+
+--#9
+/*
+Give a distinct list of the stops which may be reached from 'Craiglockhart' by taking one bus, including 'Craiglockhart' itself. Include the company and bus no. of the relevant services.
+*/
+SELECT stopa.name, a.company, a.num
+FROM route a
+  JOIN route b ON (a.num=b.num AND a.company=b.company)
+  JOIN stops stopa ON (a.stop=stopa.id)
+  JOIN stops stopb ON (b.stop=stopb.id)
+WHERE stopb.name = 'Craiglockhart'
+
+--#10
+/*
+Find the routes involving two buses that can go from Craiglockhart to Sighthill.
+Show the bus no. and company for the first bus, the name of the stop for the transfer,
+and the bus no. and company for the second bus.
+*/
+SELECT DISTINCT a.num, a.company, stopb.name ,  c.num,  c.company
+FROM route a JOIN route b
+ON (a.company = b.company AND a.num = b.num)
+JOIN ( route c JOIN route d ON (c.company = d.company AND c.num= d.num))
+JOIN stops stopa ON (a.stop = stopa.id)
+JOIN stops stopb ON (b.stop = stopb.id)
+JOIN stops stopc ON (c.stop = stopc.id)
+JOIN stops stopd ON (d.stop = stopd.id)
+WHERE  stopa.name = 'Craiglockhart' AND stopd.name = 'Sighthill'
+            AND  stopb.name = stopc.name
+ORDER BY LENGTH(a.num), b.num, stopb.id, LENGTH(c.num), d.num
