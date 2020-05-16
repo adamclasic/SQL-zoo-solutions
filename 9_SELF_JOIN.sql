5d473e1ae6b2626b38a7401bc33157e553b3dd2f
adam <aabou9@gmail.com>
2020-05-16 05:28:39 +0100

    add 5

--- 9_SELF_JOIN.sql
+++ 9_SELF_JOIN.sql
@@ -12,3 +12,15 @@ SELECT id, name
 FROM stops JOIN route ON (id = route.stop)
 WHERE num = 4 AND company = 'LRT'
 ORDER BY pos;
+-- 4. Add a HAVING clause to restrict the output to these two routes.
+SELECT company, num, COUNT(*)
+FROM route
+  WHERE stop=149 OR stop=53
+  GROUP BY company, num
+  HAVING COUNT(*) = 2;
+
+-- 5. Execute the self join shown and observe that b.stop gives all the places you can get to from Craiglockhart, without changing routes. Change the query so that it shows the services from Craiglockhart to London Road.
+SELECT a.company, a.num, a.stop, b.stop
+FROM route a JOIN route b ON
+  (a.company=b.company AND a.num=b.num)
+  WHERE a.stop=53 AND b.stop = 149;
