56640967636c502bb60bed9c8caf12ce5e40ebaa
adam <aabou9@gmail.com>
2020-05-16 05:30:06 +0100

    add 8

--- 9_SELF_JOIN.sql
+++ 9_SELF_JOIN.sql
@@ -44,3 +44,13 @@ FROM route a JOIN route b ON
   WHERE stopa.name='Haymarket'
     AND stopb.name='Leith'
   GROUP BY a.num;
+  
+  -- 8. Give a list of the services which connect the stops 'Craiglockhart' and 'Tollcross'
+SELECT a.company, a.num
+FROM route a JOIN route b ON
+  (a.company=b.company AND a.num=b.num)
+    JOIN stops stopa ON (a.stop=stopa.id)
+    JOIN stops stopb ON (b.stop=stopb.id)
+  WHERE stopa.name='Craiglockhart'
+    AND stopb.name='Tollcross'
+  GROUP BY a.num;
