a7dbdaea68dfa0d292c151f291c64285ed032356
Adam allalou <45124224+adamclasic@users.noreply.github.com>
2020-05-17 06:08:15 +0200

    Update 9_SELF_JOIN.sql

--- 9_SELF_JOIN.sql
+++ 9_SELF_JOIN.sql
@@ -62,3 +62,18 @@ FROM route a JOIN route b ON
     JOIN stops stopa ON (a.stop=stopa.id)
     JOIN stops stopb ON (b.stop=stopb.id)
         AND stopa.name='Craiglockhart';
+        
+        -- 10. Find the routes involving two buses that can go from Craiglockhart to Lochend. Show the bus no. and company for the first bus, the name of the stop for the transfer, and the bus no. and company for the second bus.
+SELECT a.num, a.company, 
+             stopc.name ,  c.num,  c.company
+FROM route a JOIN route b ON
+  (a.company=b.company AND a.num=b.num)
+  JOIN stops stopa ON (a.stop=stopa.id)
+  JOIN stops stopb ON (b.stop=stopb.id)
+             JOIN (route c JOIN route d ON (c.company=d.company AND c.num=d.num))
+  JOIN stops stopc ON (c.stop=stopc.id)
+  JOIN stops stopd ON (d.stop=stopd.id)
+  WHERE stopa.name='Craiglockhart'
+    AND stopd.name='Lochend'
+    AND stopb.name = stopc.name
+  ORDER BY a.num, a.company, stopc.name ,  c.num,  c.company;
